3c04a88f071e489054f1ba2b0f47c43c30c1d0a7
Julius Sohn <juliussohn@icloud.com>
2015-05-06 12:06:03 +0200

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            options: {
+                includePaths: ['bower_components/foundation/scss']
+            },
+            dist: {
+                options: {
+                    outputStyle: 'compressed'
+                },
+                files: {
+                    'css/app.css': 'scss/app.scss'
+                }
+            }
+        },
+        watch: {
+            grunt: {
+                files: ['Gruntfile.js']
+            },
+            sass: {
+                files: 'scss/**/*.scss',
+                tasks: ['sass'/*,'autoprefixer'*/],
+               
+            },
+       
+        },
+        bower_concat: {
+            all: {
+                dest: 'js/libs.js',
+                cssDest: 'css/libs.css',
+                dependencies: {
+                    'foundation': 'jquery',
+                },
+                mainFiles: {
+                    'foundation': 'js/foundation.js',
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-bower-concat');
+    grunt.registerTask('build', ['sass']);
+    grunt.registerTask('default', ['build', 'watch']);
+}
