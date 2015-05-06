e23482d696ed7c0213615f020a39fe9c36a7d844
Julius Sohn <juliussohn@icloud.com>
2015-05-07 00:03:46 +0200

    AHJADK

--- js/app.js
+++ js/app.js
@@ -8,43 +8,32 @@ $(document).ready(function() {
         min: 0,
         max: 100,
         val:50 
-    });
+    });*/
+	var rp1 = radialProgress($(".graph-1"))
+                .label("RADIAL 1")
+                .diameter(150)
+                .value(78)
+                .render();
 
     $(".increase-button").click(function() {
-		var to = graph_1.val();
-		  for( v=0; v<=to; v++ ){
-		    graph_1.val( v ).trigger( 'change' );
-		    console.log( v );
-		    setTimeout(100);
-		  }
+    		var data = [
+			    {
+			        value: $(this).data("productivity-increase"),
+			        color:"#F7464A",
+			        highlight: "#FF5A5E",
+			        label: "Red"
+			    },
+
+			    {
+			        value: 100-$(this).data("productivity-increase"),
+			        color: "white",
+			    }
+			];
+			myDoughnutChart.Doughnut(data,{dynamicDisplay : true});
 		
-	})*/
-var ctx = $("#myChart").get(0).getContext("2d");
-// This will get the first returned node in the jQuery collection.
+		
+	})
 
-
-var data = [
-    {
-        value: 300,
-        color:"#F7464A",
-        highlight: "#FF5A5E",
-        label: "Red"
-    },
-    {
-        value: 50,
-        color: "#46BFBD",
-        highlight: "#5AD3D1",
-        label: "Green"
-    },
-    {
-        value: 100,
-        color: "#FDB45C",
-        highlight: "#FFC870",
-        label: "Yellow"
-    }
-]
-
-var myDoughnutChart = new Chart(ctx).Doughnut(data);
 
 });
 
